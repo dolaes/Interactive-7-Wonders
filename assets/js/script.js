9e2180d146dd1f920965a317acc703cbee0834ae
Heisenberg <esfahani.cyrus@gmail.com>
2024-07-22 21:23:13 -0700

    Added all the images for the 7 wonders of the world and included swiper for functionality of the gallary

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const allGalaries = document.querySelectorAll('.mySwiper');
+for (let i= 0 ; i <allGalaries.length ; i++){
+    var swiper = new Swiper(allGalaries[i], {
+        slidesPerView: 3.5,
+        spaceBetween: 10,
+        mousewheel: true,
+        
+        centeredSlides: true,
+        keyboard: {
+          enabled: true,
+        },
+        freeMode: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+             992: {
+              slidesPerView: 4.5,
+              spaceBetween: 10,
+            },
+           1200: {
+              slidesPerView: 5.5,
+              spaceBetween: 20,
+            },
+        },
+      });
+
+}
